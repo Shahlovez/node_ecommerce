1e20f821a3f1fe9131047883916e97aafbd9dc78
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-10 23:50:38 -0400

    Serving products

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-const express = require('express')
+const express = require('express');
+const products = require ('./data/products')
 
 const app = express()
 
-app.get('/')
+app.get('/', (req, res) => {
+    res.send ('API is running...')
+})
 
-
-
-
-
+app.get('/api/products', (req, res) =>{
+    res.json(products)
+})
+app.get('/api/products/:id', (req, res) =>{
+    const product = products.find ((p) => p._d === req.params.id)
+    res.json(products)
+})
 
 
 app.listen(5000, console.log('Server running on port 5000'))
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
+// import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
  <Navbar bg="light" expand="lg" collapseOnSelect>
   <Container>
-  {/* const icon = (
-    <span class='logo'>
-    <a href="/"><img src='techshop.png' alt='logo' style={{width:110, marginTop:-18, marginBottom:-28}} /></a> 
-    </span>
-  ) */}
+{/* <LinkContainer to='/'> */}
 
-  <LinkContainer to='/'>
+{/* <Nav.Link as={Link} to="/path">children</Nav.Link> */}
     <Navbar.Brand>
-     {/* <a href="/"><img src='techshop.png' alt='logo' style={{width:110, marginTop:-18, marginBottom:-28}} /></a>  */}
+     <a href="/"><img src='techshop.png' alt='logo' style={{width:110, marginTop:-18, marginBottom:-28}} /></a> 
     </Navbar.Brand>
-    </LinkContainer>
+   {/* </LinkContainer> */}
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="justify-content-end" style={{ width: "100%" }}>
-    <LinkContainer to='/cart'>
+    {/* <LinkContainer to='/cart'> */}
         <Nav.Link>
         <i className='fas fa-shopping-cart'></i>Cart
         </Nav.Link>
-      </LinkContainer>
-      <LinkContainer to='/login'>
+      {/* </LinkContainer> */}
+      {/* <LinkContainer to='/login'> */}
         <Nav.Link>
         <i className='fas fa-user'></i>Sign In
         </Nav.Link>
-        </LinkContainer>
+        {/* </LinkContainer> */}
       </Nav>
     </Navbar.Collapse>
   </Container>
