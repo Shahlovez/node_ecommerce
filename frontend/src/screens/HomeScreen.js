da8da6b9ff1155781cd1749da31fa2cb1948d3b3
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-11 01:00:16 -0400

    Nodemon and Concurrently setup

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = () => {
   const [products, setProducts] = useState([])
   
 useEffect(() => {
-  const fetchProducts = async () =>{
+  const fetchProducts = async() =>{
     const { data } = await axios.get('/api/products')
 
     setProducts(data)
