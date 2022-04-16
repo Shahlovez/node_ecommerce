8eec78c41e87141aa6a96df70092a46749bf26c2
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-16 06:03:47 -0400

    Added colors and fixed server bug

--- backend/config /db.js
+++ backend/config /db.js
@@ -5,11 +5,11 @@ const connectDB = async () => {
         const conn = await mongoose.connect (process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
-            useCreateIndex: true,
+            // useCreateIndex: true,
         })
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
-        console.log (`Error: ${error.message}`)
+        console.log (`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
