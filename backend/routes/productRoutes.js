0fd91dddc904922ad4c64e4abc0359efaf8d9266
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-18 00:49:01 -0400

    Updated

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,23 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import {
-  getProducts,
-  getProductById,
-  deleteProduct,
-  createProduct,
-  updateProduct,
-  createProductReview,
-  getTopProducts,
-} from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+import Product from '../models/productModel.js'
 
-router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id/reviews').post(protect, createProductReview)
-router.get('/top', getTopProducts)
-router
-  .route('/:id')
-  .get(getProductById)
-  .delete(protect, admin, deleteProduct)
-  .put(protect, admin, updateProduct)
+router.get('/', asyncHandler(async (req, res)=> {
+    const products = await Product.find ({})
+    res.json(products)
+
+}))
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message: 'Product Not found'})
+    }
+    
+}))
 
 export default router
