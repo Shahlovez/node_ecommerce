a150f9d32102a2209a50814f9e6e7854674b4218
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-21 06:15:17 -0400

    Added message component

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,8 +6,8 @@ import Product from '../models/productModel.js'
 router.get('/', asyncHandler(async (req, res)=> {
     const products = await Product.find ({})
     res.json(products)
-
-}))
+})
+)
 
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
