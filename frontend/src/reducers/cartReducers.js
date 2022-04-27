20f6ff52c994a51cb36ca2d977ea368013b76a2b
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-27 12:24:27 -0400

    Added functionality to cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -5,19 +5,21 @@ export const cartReducer = (state = { cartItems:[]}, action) => {
         case CART_ADD_ITEM:
             const item = action.payload
 
-            const existItem = state.cartItems.find((x) => x.product === item.product)
+            const existItem = state.cartItems.find(x => x.product === item.product)
 
             if (existItem){
                 return {
                     ...state,
-                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item: x),
+                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item: x)
                 }
               }  else {
                         return {
                         ...state,
                         cartItems: [...state.cartItems, item]
-                        }
+                        } 
                     }
+                default:
+                    return state
                 }
             }
     
