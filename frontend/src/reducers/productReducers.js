6ea0d6a831b7c6f034d7564414eb1c44064d94fd
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-27 10:55:12 -0400

    Corrected the products

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -11,10 +11,10 @@ import {
 export const productListReducer = (state = { products: []} , action) => {
     switch (action.type) {
       case PRODUCT_LIST_REQUEST:
-        return { loading: true, products: [] }
+        return { loading: true, products:[]}
       case PRODUCT_LIST_SUCCESS:
         return {
-          loading: false, error: action.payload}
+          loading: false, products: action.payload}
       case PRODUCT_LIST_FAIL:
         return { loading: false, error: action.payload }
       default:
