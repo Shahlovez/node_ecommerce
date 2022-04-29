2da37447bd0b789913b406edbacb2139fcf2ae40
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-29 10:28:43 -0400

    Updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,23 @@ import User from '../models/userModel.js'
 
 const authUser = asyncHandler(async(req, res) =>{
     const { email, password } = req.body
-    res.send (email, password)
-})
+  
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))){
+        res.json ({
+            __id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+          })
+        } else {
+          res.status(401)
+          throw new Error('Invalid email or password')
+        }
+      })
+
+
 
 export { authUser }
