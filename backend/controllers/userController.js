d8a2e4fd89fe4ce580b3eaba9948a56f70d92266
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-29 10:10:50 -0400

    Updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,5 +2,8 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
 const authUser = asyncHandler(async(req, res) =>{
-  
+    const { email, password } = req.body
+    res.send (email, password)
 })
+
+export { authUser }
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ dotenv.config()
 connectDB ()
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send ('API is running...')
 })
