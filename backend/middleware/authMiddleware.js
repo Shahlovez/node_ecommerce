bf3fbe913cd257177aeccc4ac0e5ea7dbd9ebba1
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-19 21:47:04 -0400

    Updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,9 +12,20 @@ const protect = asyncHandler (async (req, res, next) => {
      try{
         token = req.headers.authorization.split(' ')[1]
          const decoded = jwt.verify(token, process.env.JWT_SECRET)
-     }catch(error){}
+
+     req.user = await User.findById(decoded.id).select('-password')
+
+         next()
+     }catch(error){
+         console.error(error)
+         res.status(401)
+         throw new Error('Not authorized, token failed')
+     }
     }
-    
+    if(!token){
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
 
 })
 
