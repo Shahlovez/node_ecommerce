973723c192c83b71f8d370cc55876112ad3bc826
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-19 22:29:08 -0400

    Added a middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
 
-const protect = asyncHandler (async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
     let token
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')
@@ -29,4 +29,4 @@ const protect = asyncHandler (async (req, res, next) => {
 
 })
 
-export { protect }
+export {protect}
