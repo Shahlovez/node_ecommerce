3f76486e3b2d5fcf1284ca4bf31a6c10d14af079
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-23 12:01:42 -0400

    Added the login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {Container} from 'react-bootstrap'; 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
@@ -13,7 +13,7 @@ const App = () => {
     <Header />
     <main className='py-3'>
     <Container>
-        <Route path='/' component={HomeScreen} exact />
+        <Route path='/login' component={LoginScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
     </Container>
