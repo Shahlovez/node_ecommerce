da8452ef10576849838f62b17c08f6183490bdec
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-23 11:16:57 -0400

    Created login screen

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import {login} from '../actions/userActions'
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+  return (
+    <div></div>
+  )
+}
+
+export default LoginScreen
