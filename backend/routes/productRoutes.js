7bbc625a9c563501c61044eb241fad80ef913299
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-25 11:38:21 -0400

    Imported authMiddleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router()
 import {
     getProducts,
     getProductById, } from '../controllers/productController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
-    router.route('/').get(getProducts).post(protect, admin, createProduct)
+    router.route('/').get(getProducts).post(protect)
 
     router
     .route('/:id')
     .get(getProductById)
-    .delete(protect, admin, deleteProduct)
-    .put(protect, admin, updateProduct)
+   
   
 export default router
