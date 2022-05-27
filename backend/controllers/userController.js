34e7aad3a293366ff4b693e2ec95d12e9f15bfe4
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-05-27 12:17:35 -0400

    Added update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,22 +51,45 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   })
 
-      const getUserProfile = asyncHandler(async(req, res) =>{   
-            const user = await User.findById(req.user._id)
-    
-            if (user && (await user.matchPassword(password))){
-                res.json ({
-                    __id: user._id,
-                    name: user.name,
-                    email: user.email,
-                    isAdmin: user.isAdmin,
-                    token: generateToken(user._id),
-                })
-                } else {
-                res.status(401)
-                throw new Error('Invalid email or password')
-                }
-            })
+const getUserProfile = asyncHandler(async(req, res) =>{   
+      const user = await User.findById(req.user._id)
 
+      if (user && (await user.matchPassword(password))){
+          res.json ({
+              __id: user._id,
+              name: user.name,
+              email: user.email,
+              isAdmin: user.isAdmin,
+              token: generateToken(user._id),
+          })
+          } else {
+          res.status(401)
+          throw new Error('Invalid email or password')
+          }
+      })
 
-export { authUser, registerUser, getUserProfile }
+const updateUserProfile = asyncHandler(async(req, res) =>{   
+  const user = await User.findById(req.user._id)
+
+  if (user){
+   user.name = req.body.name || user.name
+   user.email = req.body.email || user.email
+   if (req.body.password){
+     user.password = req.body.password
+   }
+
+   const updatedUser = await user.save()
+   
+   res.json ({
+    __id: updatedUser._id,
+    name: updatedUser.name,
+    email: updatedUser.email,
+    isAdmin: updatedUser.isAdmin,
+    token: generateToken(updatedUser._id),
+  })
+  } else {
+      res.status(404)
+      throw new Error('User not found')
+      }
+  })
+export { authUser, registerUser, getUserProfile, updateUserProfile }
