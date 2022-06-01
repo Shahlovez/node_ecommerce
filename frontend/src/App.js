ecc34c660c4d75c9b334852db7fc54058408000d
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-01 11:28:05 -0400

    Imported profile screen and created a route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
     <Container>
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
+        <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
         <Route path='/' component={HomeScreen} exact />
