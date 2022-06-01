d036c2c2f0778ecdd8e1ec7e615e87f0d03815cc
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-01 10:27:24 -0400

    Added user details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,11 +1,14 @@
 import { 
-    USER_LOGIN_FAIL, 
-    USER_LOGIN_REQUEST, 
-    USER_LOGIN_SUCCESS,
-    USER_LOGOUT,
-    USER_REGISTER_FAIL,
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_LOGIN_FAIL, 
+  USER_LOGIN_REQUEST, 
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
  } from "../constants/userConstants"
 
 export const userLoginReducer = (state = { } , action) => {
@@ -23,13 +26,26 @@ export const userLoginReducer = (state = { } , action) => {
     }
   }
 
-  export const userRegisterReducer = (state = { } , action) => {
+  export const userRegisterReducer = (state = { }, action) => {
     switch (action.type) {
       case USER_REGISTER_REQUEST:
         return { loading: true }
       case USER_REGISTER_SUCCESS:
         return { loading: false, userInfo: action.payload}
       case USER_REGISTER_FAIL:
+        return { loading: false, error: action.payload }
+      default:
+        return state
+    }
+  }
+
+  export const userDetailsReducer = (state = {user:{} } , action) => {
+    switch (action.type) {
+      case USER_DETAILS_REQUEST:
+        return {...state, loading: true }
+      case USER_DETAILS_SUCCESS:
+        return { loading: false, user: action.payload}
+      case USER_DETAILS_FAIL:
         return { loading: false, error: action.payload }
       default:
         return state
