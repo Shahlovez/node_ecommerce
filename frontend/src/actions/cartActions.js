69c1e3e7435a64201206ebde76c1c7f2bc512576
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-03 11:06:57 -0400

    Updated

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+   CART_ADD_ITEM, 
+   CART_REMOVE_ITEM, 
+   CART_SAVE_SHIPPING_ADDRESS 
+  } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const {data} = await axios.get(`/api/products/${id}`)
