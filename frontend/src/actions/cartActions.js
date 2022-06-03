4b9b29122981852daa6d0096b443bab7573ef50c
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-03 10:48:19 -0400

    Updated with save shipping address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -26,4 +26,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       
         localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
       }
+  
+    export const saveShippingAddress = (data) => (dispatch) => {
+        dispatch({
+          type: CART_SAVE_SHIPPING_ADDRESS,
+          payload: data,
+        })
+      
+        localStorage.setItem('shippingAddress', JSON.stringify(data))
+      }
       
