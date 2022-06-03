95f742fe4144010bd5fbe3bbf08b42fcd21253ce
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-03 11:07:02 -0400

    Updated

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,13 @@ import {
     productDetailsReducer
   } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { 
+    userLoginReducer, 
+    userRegisterReducer,
+     userDetailsReducer, 
+     userUpdateProfileReducer, 
+    
+    } from './reducers/userReducers'
 
 const reducer = combineReducers ({
     productList: productListReducer,
@@ -22,8 +28,10 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse (loc
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse (localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse (localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
+    cart: {cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage},
     userLogin: { userInfo: userInfoFromStorage},
 }
 const middleware = [thunk]
