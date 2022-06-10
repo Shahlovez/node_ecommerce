2af7bface6043b1c79aa2771311b09eb76210a71
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-09 22:55:45 -0400

    Updated

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -7,8 +7,27 @@ import CheckoutSteps from '../components/CheckoutSteps'
 
 
 const PlaceOrderScreen = () => {
-
     const cart = useSelector(state => state.cart)
+    // Calculate prices 
+
+ const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+    }
+      
+cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty,0)
+)
+cart.shippingPrice = addDecimals(cart.itemsPrice >100 ? 0 : 100)
+cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
+
+    const placeOrderHandler = () => {
+        console.log('order')
+    }
   return (
     <>
         <CheckoutSteps step1 step2 step3 step4/>
@@ -79,12 +98,42 @@ const PlaceOrderScreen = () => {
                   <Col>${cart.itemsPrice}</Col>
                 </Row>
               </ListGroup.Item>
-
-
-
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+             
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type='button'
+                  className='btn-block'
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
             </ListGroup>
-            </Col>
-            </Row>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
