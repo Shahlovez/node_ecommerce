5ba086f25d0b4c055ae4b5a3c673a26118c4e3aa
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-17 22:36:31 -0400

    Imported order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/order/:id?' component={OrderScreen} />
         <Route path='/payment' component={PaymentScreen} />
         <Route path='/placeorder' component={PlaceOrderScreen} />
         <Route path='/shipping' component={ShippingScreen} />
