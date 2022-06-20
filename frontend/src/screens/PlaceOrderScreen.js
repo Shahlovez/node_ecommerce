d5fb15af9e9fa8af2b70de061520b49ef5b8ba31
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-19 21:26:27 -0400

    Updated

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
 import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
+import { USER_DETAILS_RESET } from '../constants/userConstants'
 
 
 const PlaceOrderScreen = ({history}) => {
