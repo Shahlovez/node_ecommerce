7050ed2b4a78decce8b4894b5c92ffc389c16f83
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-19 20:58:07 -0400

    Added pay reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,10 +5,10 @@ import {
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
-    // ORDER_PAY_REQUEST,
-    // ORDER_PAY_FAIL,
-    // ORDER_PAY_SUCCESS,
-    // ORDER_PAY_RESET,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_RESET,
     // ORDER_LIST_MY_REQUEST,
     // ORDER_LIST_MY_SUCCESS,
     // ORDER_LIST_MY_FAIL,
@@ -67,6 +67,29 @@ import {
           loading: false,
           error: action.payload,
         }
+      default:
+        return state
+    }
+  }
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+      case ORDER_PAY_REQUEST:
+        return {
+          loading: true,
+        }
+      case ORDER_PAY_SUCCESS:
+        return {
+          loading: false,
+         success: true,
+        }
+      case ORDER_PAY_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+        case ORDER_PAY_RESET:
+          return {}
       default:
         return state
     }
