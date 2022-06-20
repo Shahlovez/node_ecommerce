83f38528490541453449b8bec3feaf80c85fc2cc
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-19 20:33:50 -0400

    Added updating the order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,4 +57,29 @@ const getOrderById = asyncHandler(async(req, res) =>{
     
     
     export { addOrderItems, getOrderById }
-      
+
+// @desc update order to paid
+// @route get/api/orders/:id/pay
+// @access private
+
+const updateOrderToPaid= asyncHandler(async(req, res) =>{
+    const order = await Order.findById(req.params.id)
+      if (order) {
+      order.isPaid = true
+      order.paidAt = Date.now()
+      order.pyamentResult ={
+          id: req.body.id,
+          status: req.body.status,
+          update_time: req.body.update_time,
+          email_address: req.body.payer.email_address
+      }
+
+      const updateOrder = await order.save()
+      res.json(updateOrder)
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+    
+    export { addOrderItems, getOrderById, updateOrderToPaid }
