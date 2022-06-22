d3782f4fc78822cf3889a055a16b687dad369fe6
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-22 11:31:15 -0400

    Added PayPal script

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -35,6 +35,17 @@ if(!loading){
  }
 
   useEffect(() => {
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
+    }
       dispatch(getOrderDetails(orderId))
   }, [dispatch, orderId])
 
