c4853eb9c6396f78cf29df916c6021e4d0ef0435
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-24 12:29:26 -0400

    Added my orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,4 +82,13 @@ const updateOrderToPaid= asyncHandler(async(req, res) =>{
     }
 })
     
-    export { addOrderItems, getOrderById, updateOrderToPaid }
+// @desc get logged in user orders 
+// @route get/api/orders/myorders
+// @access private
+
+const getMyOrders= asyncHandler(async(req, res) =>{
+  const orders = await Order.find({user: req.user_id})
+  res.json(orders)
+})
+  
+    export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
