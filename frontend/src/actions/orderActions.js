401ba06e57a7dc97578aab3286293b8173ea8dfe
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-24 12:51:07 -0400

    Updated

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -9,6 +9,9 @@ import {
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS,
     ORDER_PAY_FAIL,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_FAIL,
     
 
 
@@ -19,7 +22,6 @@ import {
       dispatch({
         type: ORDER_CREATE_REQUEST,
       })
-  
       const {
         userLogin: { userInfo },
       } = getState()
@@ -126,6 +128,43 @@ import {
       }
       dispatch({
         type: ORDER_PAY_FAIL,
+        payload: message,
+      })
+    }
+  }
+
+  export const listMyOrders = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_LIST_MY_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.get(`/api/orders/myorders`, config)
+  
+      dispatch({
+        type: ORDER_LIST_MY_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: ORDER_LIST_MY_FAIL,
         payload: message,
       })
     }
