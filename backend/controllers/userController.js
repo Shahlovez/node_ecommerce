49a933b51ae6bef94f6f6fb5a0c72d75dd02ef8d
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-30 22:46:06 -0400

    Updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ const authUser = asyncHandler(async(req, res) =>{
 
     if (user && (await user.matchPassword(password))){
         res.json ({
-            __id: user._id,
+            _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
@@ -51,22 +51,25 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   })
 
+  // ********* Get user profile ********
+
 const getUserProfile = asyncHandler(async(req, res) =>{   
       const user = await User.findById(req.user._id)
 
-      if (user && (await user.matchPassword(password))){
+      if (user){
           res.json ({
-              __id: user._id,
+              _id: user._id,
               name: user.name,
               email: user.email,
               isAdmin: user.isAdmin,
-              token: generateToken(user._id),
           })
           } else {
-          res.status(401)
-          throw new Error('Invalid email or password')
+          res.status(404)
+          throw new Error('User not found')
           }
       })
+
+      //  ********** Update profile **** 
 
 const updateUserProfile = asyncHandler(async(req, res) =>{   
   const user = await User.findById(req.user._id)
@@ -81,7 +84,7 @@ const updateUserProfile = asyncHandler(async(req, res) =>{
    const updatedUser = await user.save()
    
    res.json ({
-    __id: updatedUser._id,
+    _id: updatedUser._id,
     name: updatedUser.name,
     email: updatedUser.email,
     isAdmin: updatedUser.isAdmin,
@@ -92,4 +95,11 @@ const updateUserProfile = asyncHandler(async(req, res) =>{
       throw new Error('User not found')
       }
   })
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+//  ******* Get all users ******* 
+
+  const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+  })
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
