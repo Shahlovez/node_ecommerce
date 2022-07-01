0c492eba9388ebca54e4358426b4044eececaf13
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-30 22:50:59 -0400

    Added admin

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -26,7 +26,14 @@ const protect = asyncHandler(async (req, res, next) => {
         res.status(401)
         throw new Error('Not authorized, no token')
     }
-
 })
 
-export {protect}
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next()
+    } else {
+      res.status(401)
+      throw new Error('Not authorized as an admin')
+    }
+  }
+export {protect, admin}
