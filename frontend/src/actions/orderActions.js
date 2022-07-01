6e5f9de6dbf2b8ee89c2161762d7d5dc7d54f1a2
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-06-30 23:47:08 -0400

    Updated

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
@@ -12,11 +13,14 @@ import {
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
     ORDER_LIST_MY_FAIL,
-    
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_REQUEST
 
 
   } from '../constants/orderConstants'
-
+  import { logout } from './userActions'
+  
   export const createOrder = (order) => async (dispatch, getState) => {
     try {
       dispatch({
@@ -133,6 +137,47 @@ import {
     }
   }
 
+  export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_DELIVER_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        config
+      )
+  
+      dispatch({
+        type: ORDER_DELIVER_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: ORDER_DELIVER_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
   export const listMyOrders = () => async (dispatch, getState) => {
     try {
       dispatch({
