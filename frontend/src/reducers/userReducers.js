9385743520c2ca2cf73010e997083260d7a22ec7
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-01 20:29:45 -0400

    Added user delete reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -16,7 +16,10 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
-  USER_LIST_RESET
+  USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL
 
  } from "../constants/userConstants"
 
@@ -86,6 +89,18 @@ export const userLoginReducer = (state = { } , action) => {
         return { loading: false, error: action.payload }
       case USER_LIST_RESET:
           return { users: [] }
+      default:
+        return state
+    }
+  }
+  export const userDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+      case USER_DELETE_REQUEST:
+        return { loading: true }
+      case USER_DELETE_SUCCESS:
+        return { loading: false, success: true }
+      case USER_DELETE_FAIL:
+        return { loading: false, error: action.payload }
       default:
         return state
     }
