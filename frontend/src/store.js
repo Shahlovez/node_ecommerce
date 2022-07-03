332d9bb87b6f9a8f983a1a4079bc25e9a5425c37
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-02 23:37:38 -0400

    Updated

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,8 @@ import {
     userDetailsReducer, 
     userUpdateProfileReducer, 
     userListReducer,
-    userDeleteReducer
+    userDeleteReducer,
+    userUpdateReducer,
     
     } from './reducers/userReducers'
 import { 
@@ -32,6 +33,7 @@ const reducer = combineReducers ({
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
