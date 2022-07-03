061feee15aa71f8ccec595540294fbd346e3c87d
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-02 23:10:04 -0400

    Updated routes with user by ID and updating user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,8 +7,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
-//   getUserById,
-//   updateUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -21,7 +21,7 @@ router
 router
   .route('/:id')
   .delete(protect, admin, deleteUser)
-//   .get(protect, admin, getUserById)
-//   .put(protect, admin, updateUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
