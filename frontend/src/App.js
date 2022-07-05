42310be5f15346de44d909cb5213d556fd7483ff
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-04 23:12:35 -0400

    Imported product list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,8 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
-import UserEditScreen from './screens/UserEditScreen';
+import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen';
 
 
 const App = () => {
@@ -33,6 +34,7 @@ const App = () => {
         <Route path='/placeorder' component={PlaceOrderScreen} />
         <Route path='/shipping' component={ShippingScreen} />
         <Route path='/admin/userlist' component={UserListScreen}/>
+        <Route path='/admin/productlist' component={ProductListScreen}/>
         <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
         <Route path='/' component={HomeScreen} exact />
     </Container>
