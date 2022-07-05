e4c08eedc0354c7f78b2a789e2310d8e46766848
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-05 00:02:48 -0400

    Updated

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -7,8 +7,8 @@ import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
 import {
   listProducts,
-//   deleteProduct,
-  createProduct,
+  deleteProduct,
+//   createProduct,
 } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
@@ -27,13 +27,13 @@ const ProductListScreen = ({ history, match }) => {
     success: successDelete,
   } = productDelete
 
-  const productCreate = useSelector((state) => state.productCreate)
-  const {
-    loading: loadingCreate,
-    error: errorCreate,
-    success: successCreate,
-    product: createdProduct,
-  } = productCreate
+//   const productCreate = useSelector((state) => state.productCreate)
+//   const {
+//     loading: loadingCreate,
+//     error: errorCreate,
+//     success: successCreate,
+//     product: createdProduct,
+//   } = productCreate
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
@@ -60,11 +60,11 @@ const ProductListScreen = ({ history, match }) => {
     pageNumber,
   ])
 
-//   const deleteHandler = (id) => {
-//     if (window.confirm('Are you sure')) {
-//       dispatch(deleteProduct(id))
-//     }
-//   }
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+      dispatch(deleteProduct(id))
+    }
+  }
 
   const createProductHandler = () => {
     dispatch(createProduct())
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,9 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {
     productListReducer,
-    productDetailsReducer
+    productDetailsReducer,
+    productDeleteReducer,
+    
   } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import { 
@@ -26,6 +28,7 @@ import {
 const reducer = combineReducers ({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
