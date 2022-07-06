f8369e63c3fbbb8720e5d7f7493c9594ef4f8290
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-05 23:01:30 -0400

    Added update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,5 +52,36 @@ const getProductById = asyncHandler(async(req, res) =>{
       res.status(201).json(createdProduct)
     })  
     
+    // ******* Update a product *******
+    const updateProduct = asyncHandler(async (req, res) => {
+        const {
+          name,
+          price,
+          description,
+          image,
+          brand,
+          category,
+          countInStock,
+        } = req.body
+      
+        const product = await Product.findById(req.params.id)
+      
+        if (product) {
+          product.name = name
+          product.price = price
+          product.description = description
+          product.image = image
+          product.brand = brand
+          product.category = category
+          product.countInStock = countInStock
+      
+          const updatedProduct = await product.save()
+          res.json(updatedProduct)
+        } else {
+          res.status(404)
+          throw new Error('Product not found')
+        }
+      })
+      
 
- export {getProducts, getProductById, deleteProduct, createProduct, }
+ export {getProducts, getProductById, deleteProduct, createProduct, updateProduct }
