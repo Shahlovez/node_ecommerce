3a97363bfa59d7cd8b54c2f407f47b8a37490166
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-08 23:21:05 -0400

    Added product update

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,6 +12,10 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 
 } from '../constants/productConstants'
 
@@ -72,3 +76,19 @@ export const productDetailsReducer = (state = { product: {reviews:[]} }, action)
             return state
         }
       }
+
+      export const productUpdateReducer = (state = { product: {}}, action) => {
+        switch (action.type) {
+          case PRODUCT_UPDATE_REQUEST:
+              return { loading: true }
+            case PRODUCT_UPDATE_SUCCESS:
+              return {
+                loading: false, success: true, product: action.payload}
+            case PRODUCT_UPDATE_FAIL:
+              return { loading: false, error: action.payload }
+              case PRODUCT_UPDATE_RESET:
+                return { product: {}}
+            default:
+              return state
+          }
+        }
