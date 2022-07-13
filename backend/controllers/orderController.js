ef7956789c9aadef6477d0b32eb24b473a04b4f2
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-12 23:07:33 -0400

    Updated order to delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -78,6 +78,26 @@ const updateOrderToPaid= asyncHandler(async(req, res) =>{
         throw new Error('Order not found')
     }
 })
+
+
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
     
 // @desc get logged in user orders 
 // @route get/api/orders/myorders
@@ -96,4 +116,10 @@ const getOrders= asyncHandler(async(req, res) =>{
   const orders = await Order.find({}).populate('user', 'id name')
   res.json(orders)
 })
-    export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders }
+    export { 
+      addOrderItems, 
+      getOrderById,
+      updateOrderToPaid, 
+      updateOrderToDelivered,
+      getMyOrders, 
+      getOrders }
