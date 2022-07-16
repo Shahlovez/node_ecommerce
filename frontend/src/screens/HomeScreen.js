af34c7817e97325a3c99100808b986af9998e5a0
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-16 00:15:13 -0400

    Updated

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,16 +5,20 @@ import Product from '../components/Product';
 import Loader from '../components/Loader'; 
 import Message from '../components/Message'; 
 import { listProducts } from '../actions/productActions';
+import { keyword } from 'chalk';
 
-const HomeScreen = () => {
-  const dispatch = useDispatch()
+const HomeScreen = (match) => {
+
+const keyword = match.params.keyword
+
+const dispatch = useDispatch()
 
 const productList = useSelector(state => state.productList)
 const { loading, error, products } = productList
   
 useEffect(() => {
-  dispatch(listProducts())
-  }, [dispatch])
+  dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
