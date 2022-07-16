e763d82d7bc74bf9b42f6457bd05323881bc5392
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-15 23:59:30 -0400

    Updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import React from 'react'
 import { Route } from 'react-router-dom'
+import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import {LinkContainer} from 'react-router-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
 import { logout } from '../actions/userActions'
+import SearchBox from './SearchBox'
 
 
 const Header = () => {
@@ -26,6 +27,7 @@ const Header = () => {
 
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
+    <Route/>
     <Nav className="justify-content-end" style={{ width: "100%" }}>
      <LinkContainer to='/cart'> 
         <Nav.Link>
