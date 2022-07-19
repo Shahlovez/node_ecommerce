b859a778c858ecb7c8026d4b15cc1582fc983484
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-18 23:28:22 -0400

    Added page size

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,8 @@ import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async(req, res) =>{
-   
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1 
   const keyword = req.query.keyword ? {
         name: {
           $regex: req.query.keyword,
@@ -10,9 +11,12 @@ const getProducts = asyncHandler(async(req, res) =>{
         },
       } : {}
 
-  const products = await Product.find({...keyword })
+  const count = await Product.countDocuments({ ...keyword })
+  const products = await Product.find({ ...keyword })
+  .limit(pageSize)
+  .skip(pageSize * (page - 1))
 
-res.json(products)
+res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 const getProductById = asyncHandler(async(req, res) =>{
