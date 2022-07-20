4ebfa67243a4b3c495a3e41a3dd47942d6662336
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-20 01:58:38 -0400

    Created a paginate file

---
+++ frontend/src/screens/Paginate.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Pagination } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <NavLink
+            key={x + 1}
+            to={
+              !isAdmin
+                ? keyword
+                  ? `/search/${keyword}/page/${x + 1}`
+                  : `/page/${x + 1}`
+                : `/admin/productlist/${x + 1}`
+            }
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </NavLink>
+        ))}
+      </Pagination>
+    )
+  )
+}
+
+export default Paginate
+Footer
