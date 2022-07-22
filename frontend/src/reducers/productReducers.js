239193ce7028fb321f73495a779b7eedee22a1a2
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-07-21 23:44:24 -0400

    Created a route for top products

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -20,6 +20,9 @@ import {
   PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_REVIEW_FAIL,
   PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_SUCCESS,
+  PRODUCT_TOP_FAIL,
 
 } from '../constants/productConstants'
 
@@ -69,50 +72,62 @@ export const productDetailsReducer = (state = { product: {reviews:[]} }, action)
       }
     }
 
-    export const productCreateReducer = (state = {}, action) => {
-      switch (action.type) {
-        case PRODUCT_CREATE_REQUEST:
-            return { loading: true }
-          case PRODUCT_CREATE_SUCCESS:
-            return {
-              loading: false, success: true, product: action.payload}
-          case PRODUCT_CREATE_FAIL:
-            return { loading: false, error: action.payload }
-            case PRODUCT_CREATE_RESET:
-              return {}
-          default:
-            return state
-        }
-      }
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+        return { loading: true }
+      case PRODUCT_CREATE_SUCCESS:
+        return {
+          loading: false, success: true, product: action.payload}
+      case PRODUCT_CREATE_FAIL:
+        return { loading: false, error: action.payload }
+        case PRODUCT_CREATE_RESET:
+          return {}
+      default:
+        return state
+    }
+  }
 
-      export const productUpdateReducer = (state = { product: {}}, action) => {
-        switch (action.type) {
-          case PRODUCT_UPDATE_REQUEST:
-              return { loading: true }
-            case PRODUCT_UPDATE_SUCCESS:
-              return {
-                loading: false, success: true, product: action.payload}
-            case PRODUCT_UPDATE_FAIL:
-              return { loading: false, error: action.payload }
-              case PRODUCT_UPDATE_RESET:
-                return { product: {}}
-            default:
-              return state
-          }
-        }
-    
-        export const productReviewCreateReducer = (state = {}, action) => {
-          switch (action.type) {
-            case PRODUCT_CREATE_REVIEW_REQUEST:
-              return { loading: true }
-            case PRODUCT_CREATE_REVIEW_SUCCESS:
-              return { loading: false, success: true }
-            case PRODUCT_CREATE_REVIEW_FAIL:
-              return { loading: false, error: action.payload }
-            case PRODUCT_CREATE_REVIEW_RESET:
-              return {}
-            default:
-              return state
-          }
-        }
-        
+  export const productUpdateReducer = (state = { product: {}}, action) => {
+    switch (action.type) {
+      case PRODUCT_UPDATE_REQUEST:
+          return { loading: true }
+        case PRODUCT_UPDATE_SUCCESS:
+          return {
+            loading: false, success: true, product: action.payload}
+        case PRODUCT_UPDATE_FAIL:
+          return { loading: false, error: action.payload }
+          case PRODUCT_UPDATE_RESET:
+            return { product: {}}
+        default:
+          return state
+      }
+    }
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true }
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {}
+    default:
+      return state
+  }
+}
+
+export const productTopRatedReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_TOP_REQUEST:
+      return { loading: true, products: [] }
+    case PRODUCT_TOP_SUCCESS:
+      return { loading: false, products: action.payload }
+    case PRODUCT_TOP_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+    }
